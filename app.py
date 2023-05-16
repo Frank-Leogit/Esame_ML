871952b05b8a01ebcec9393221e86078e42e535a
Frank-Leogit <jeanoux@live.it>
2023-05-16 11:11:48 +0200

    Finale

--- app.py
+++ app.py
@@ -16,36 +16,41 @@ import warnings
 
 
 def main():
-    st.success('This is a success message!')
-    st.markdown(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">Buonasera</p>', unsafe_allow_html=True)
-    # warnings.filterwarnings('ignore')
-    # absolute_path = os.path.dirname(__file__)
-    # relative_path = "src/lib"
-    # full_path = os.path.join(absolute_path)
-    # model = joblib.load("regression_test.pkl")
-    # x1 = st.number_input("Inserisci",,)
-    # x2 = st.number_input("Inserisci",,)
-    # x3 = st.number_input("Inserisci",,)
-    # y_pred=model.predict([[x1,x2,x3]])[0]
-    # st.write("Predizione = ",y_pred)
-    # df_res=pd.DataFrame(data = [y_pred,y_test])
-    # Y = pd.DataFrame(y_pred,columns=[f"predizione {target}"])
+    warnings.filterwarnings('ignore')
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "src/lib"
+    full_path = os.path.join(absolute_path)
+    model = joblib.load("regression_test.pkl")
+    # I valori di massimo e minimo sono stati ottenuti dal describe
+    x1= st.number_input("Inserisci il valore di rm",3.56,8.78,6.4,0.1)
+    x2 = st.number_input("Inserisci il valore di ptratio",12.6,22.1,16.9,0.1)
+    x3 = st.number_input("Inserisci lstat",1.73,37.97,6.4,0.1)
+    y_pred=model.predict([[x1,x2,x3]])[0]
+    st.write("Predizione = ",y_pred)
+    #df_res=pd.DataFrame(data = [y_pred,y_test])
+    #Y = pd.DataFrame(y_pred,columns=[f"predizione {target}"])
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        df=pd.read_csv(uploaded_file)
+        df=df[['rm', 'ptratio', 'lstat']]
+        st.write(df)
+        y_pred=model.predict(df.to_numpy())
+        res = pd.DataFrame(y_pred,columns=["Predizione"])
+        st.write(res)
+    buffer = io.BytesIO()
+    # download button 2 to download dataframe as xlsx
+    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+        # Write each dataframe to a different worksheet.
+        res.to_excel(writer, sheet_name='Sheet1', index=False)
+        # Close the Pandas Excel writer and output the Excel file to the buffer
+        writer.save()
 
-
-    # buffer = io.BytesIO()
-    # # download button 2 to download dataframe as xlsx
-    # with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-    #     # Write each dataframe to a different worksheet.
-    #     Y.to_excel(writer, sheet_name='Sheet1', index=False)
-    #     # Close the Pandas Excel writer and output the Excel file to the buffer
-    #     writer.save()
-
-    #     download2 = st.download_button(
-    #         label="Download data as Excel",
-    #         data=buffer,
-    #         file_name='large_df.xlsx',
-    #         mime='application/vnd.ms-excel'
-    #     )
+        download2 = st.download_button(
+            label="Download data as Excel",
+            data=buffer,
+            file_name='large_df.xlsx',
+            mime='application/vnd.ms-excel'
+        )
         
 if __name__=="__main__":
     main()
